f88006d8f2dc931889b8a2950669a9e39faf56bd
McCambley <jakemccamb16@gmail.com>
2021-04-06 14:12:35 -0400

    develop: add event listeners to keys

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # JSPlayground
 Here's my repository for all projects under the #Javascript30 umbrella
+
+## View the projects [here](https://mccambley.github.io/JSPlayground/)!
--- projects/drumkit/drumkit.js
+++ projects/drumkit/drumkit.js
@@ -1,1 +1,21 @@
-console.log("Hello!");
+const keys = document.querySelectorAll(".key");
+
+keys.forEach( key => key.addEventListener('transitionend', stopPlaying));
+
+function stopPlaying(e) {
+  if(e.propertyName !== 'transform') return;
+  this.classList.remove("playing");
+}
+
+function playSound(e) {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"`);
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"`);
+  if (!audio) return;
+
+  audio.currentTime = 0;
+  audio.play();
+
+  key.classList.add("playing");
+}
+
+window.addEventListener('keydown', playSound)
