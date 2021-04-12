c0c9eea41c466316ccbee0921d17c5c76f6d7713
McCambley <jakemccamb16@gmail.com>
2021-04-11 23:08:31 -0400

    feat: add JS functionality and responsiveness

--- panels/script.js
+++ panels/script.js
@@ -1,1 +1,23 @@
 console.log("Hello!")
+
+let panel = document.querySelectorAll(".panel");
+
+
+function openUp() {
+  this.classList.toggle('open');
+}
+
+function emojiDrop(e) {
+  if (e.propertyName.includes('flex')) {
+    let top = this.querySelector('.top');
+    let bot = this.querySelector('.bot');
+
+    top.classList.toggle('opened');
+    bot.classList.toggle('opened');
+
+    console.log('umm');
+  }
+}
+
+panel.forEach(pan => pan.addEventListener('click', openUp));
+panel.forEach(pan => pan.addEventListener('transitionend', emojiDrop));
