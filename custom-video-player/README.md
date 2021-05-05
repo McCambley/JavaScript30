4c56edee881eb84390c05c633a5022897c1eacff
McCambley <jakemccamb16@gmail.com>
2021-05-05 00:14:06 -0400

    feat: add functionality on video player inputs

---
+++ custom-video-player/README.md
@@ -0,0 +1,15 @@
+# Custom Video Player
+
+## Overview
+
+This one's cool, I like this one. We removed the default controls and used some funky yet basic CSS to get custom controls up in there. Javascript is used to link the inputs to the video playback such that we can manipulate the video just as we would be with the default controls.
+
+## Future Ideas
+
+- I'd like to take this project a step further and combine some old functionality from previous projects in order to update the volume and playback speeds more smoothly.
+
+- I'd also like to add a few radio buttons in order to allow for users to change which video they're viewing. I can't chose which video of my dog to display and I'd like to be able to see all of them. Learning in order to enable my indecision. Amazing.
+
+- Finally, gotta add a functioning full screen button.
+
+### [View the project live here!](https://mccambley.github.io/JSPlayground/custom-video-player/)
--- custom-video-player/script.js
+++ custom-video-player/script.js
@@ -1,1 +1,68 @@
 console.info("Hello, World!");
+// get items
+const player = document.querySelector(".player");
+const video = player.querySelector(".player__video");
+const controls = player.querySelector(".player__controls");
+const progress = player.querySelector(".player__progress");
+const progressBar = player.querySelector(".player__progress-watched");
+const toggle = player.querySelector(".toggle");
+const skipButtons = player.querySelectorAll("[data-skip]");
+const ranges = player.querySelectorAll(".player__slider");
+
+// build functions
+function togglePlay() {
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+}
+
+function togglePlayWithSpace(e) {
+  if ((e.key = " ")) {
+    togglePlay();
+  }
+}
+
+function updateButton() {
+  const icon = this.paused ? "▶️" : "⏸";
+}
+
+function skip() {
+  video.currentTime += parseFloat(this.dataset.skip);
+}
+
+function handleRangeUpdate() {
+  video[this.name] = this.value;
+}
+
+function handleProgress() {
+  const percent = (video.currentTime / video.duration) * 100;
+  progressBar.style.flexBasis = `${percent}%`;
+}
+
+function scrub(e) {
+  const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+  console.log(mouseDown);
+  video.currentTime = scrubTime;
+}
+
+// configure event listeners
+window.addEventListener("keydown", togglePlayWithSpace);
+video.addEventListener("click", togglePlay);
+video.addEventListener("play", updateButton);
+video.addEventListener("pause", updateButton);
+video.addEventListener("timeupdate", handleProgress);
+toggle.addEventListener("click", togglePlay);
+skipButtons.forEach((button) => {
+  button.addEventListener("click", skip);
+});
+ranges.forEach((range) => range.addEventListener("change", handleRangeUpdate));
+progress.addEventListener("click", scrub);
+progress.addEventListener("mousemove", (e) => {
+  mouseDown && scrub(e);
+});
+
+let mouseDown = false;
+progress.addEventListener("mousedown", () => (mouseDown = true));
+progress.addEventListener("mouseup", () => (mouseDown = false));
