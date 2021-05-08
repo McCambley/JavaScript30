c25f9a507ef4675f504b9864315f4b5667d45c31
McCambley <jakemccamb16@gmail.com>
2021-05-08 01:13:31 -0400

    feat: finalize styling and functionality of Key Sequence Detector project

--- sequence/script.js
+++ sequence/script.js
@@ -1,6 +1,10 @@
-console.info("Hello, World!");
+let keyLog = [];
+const secretCode = "secretcode";
+
 window.addEventListener("keydown", (e) => {
-  if (e.key === "e") {
+  keyLog.push(e.key);
+  keyLog.splice(-secretCode.length - 1, keyLog.length - secretCode.length);
+  if (keyLog.join("") === secretCode) {
     alert("You did it!!");
   }
 });
