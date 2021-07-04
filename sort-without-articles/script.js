5412f58371a642b914416afc44a360a6421a1ac2
McCambley <jakemccamb16@gmail.com>
2021-07-04 17:10:46 -0400

    Dev: setup sort project

---
+++ sort-without-articles/script.js
@@ -0,0 +1,3 @@
+const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
+const bandList = document.querySelector('.bands');
+const sortedBands = bands.sort((a, b) => {});
