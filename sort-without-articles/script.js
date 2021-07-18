95c67f0cb0542bbd9a5c8d835a195b62c5f05857
McCambley <jakemccamb16@gmail.com>
2021-07-17 20:57:58 -0400

    Dev: complete sort without articles project

--- sort-without-articles/script.js
+++ sort-without-articles/script.js
@@ -1,3 +1,28 @@
-const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
+const bands = [
+  'The Plot in You',
+  'The Devil Wears Prada',
+  'Pierce the Veil',
+  'Norma Jean',
+  'The Bled',
+  'Say Anything',
+  'The Midway State',
+  'We Came as Romans',
+  'Counterparts',
+  'Oh, Sleeper',
+  'A Skylit Drive',
+  'Anywhere But Here',
+  'An Old Dog',
+];
+
 const bandList = document.querySelector('.bands');
-const sortedBands = bands.sort((a, b) => {});
+
+// remove article form beginning of bandname
+function strip(bandName) {
+  return bandName.replace(/^(a |the |an )/i, '').trim();
+}
+
+// sort bandnames without articles
+const sortedBands = bands.sort((a, b) => (strip(a) > strip(b) ? 1 : -1));
+
+// render list to DOM
+bandList.innerHTML = sortedBands.map(band => `<li class='band'>${band}</li>`).join('');
