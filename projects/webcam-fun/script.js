5a88843b02473a42e996b8884c36657ba95c65fe
McCambley <jakemccamb16@gmail.com>
2021-07-19 01:27:15 -0400

    Dev: create take-photo functionality

--- projects/webcam-fun/script.js
+++ projects/webcam-fun/script.js
@@ -5,3 +5,47 @@ const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+const button = document.querySelector('.button');
+
+function getVideo() {
+  navigator.mediaDevices
+    .getUserMedia({ video: true, audio: false })
+    .then(localMediaStream => {
+      video.srcObject = localMediaStream;
+      video.play();
+    })
+    .catch(err => console.error(err));
+}
+
+function paintToCanvas() {
+  const width = video.videoWidth;
+  const height = video.videoHeight;
+  canvas.width = width;
+  canvas.height = height;
+  console.log(width, height);
+
+  setInterval(() => {
+    ctx.drawImage(video, 0, 0, width, height);
+  }, 100);
+}
+
+function takePhoto() {
+  // play snap sound
+  snap.currentTime = 0;
+  snap.play();
+  console.log('Say Cheese!');
+
+  // take the data from the canvas
+  const data = canvas.toDataURL('image/jpeg');
+  const link = document.createElement('a');
+  link.href = data;
+  link.setAttribute('download', 'handsome');
+  link.textContent = 'Download Image';
+  strip.insertBefore(link, strip.firstChild);
+  // console.log(data);
+}
+
+getVideo();
+
+button.addEventListener('click', takePhoto);
+video.addEventListener('canplay', paintToCanvas);
