ed3fa2ce3389137c87f75daf865cc6b536644f14
McCambley <jakemccamb16@gmail.com>
2021-09-21 23:32:30 -0400

    Dev: complete drag and drop

--- projects/click-and-drag/script.js
+++ projects/click-and-drag/script.js
@@ -1,1 +1,29 @@
-console.info("Hello, World!");
+const cards = document.querySelector('.cards');
+let isDown = false;
+let startX;
+let scrollLeft;
+
+cards.addEventListener('mousedown', (evt) => {
+  isDown = true;
+  cards.classList.add('cards_active');
+  startX = evt.pageX - cards.offsetLeft;
+  scrollLeft = cards.scrollLeft;
+});
+
+cards.addEventListener('mouseleave', () => {
+  isDown = false;
+  cards.classList.remove('cards_active');
+});
+
+cards.addEventListener('mouseup', () => {
+  isDown = false;
+  cards.classList.remove('cards_active');
+});
+
+cards.addEventListener('mousemove', (evt) => {
+  if (!isDown) return;
+  evt.preventDefault();
+  x = evt.pageX - cards.offsetLeft;
+  const walk = (x - startX) * 2;
+  cards.scrollLeft = scrollLeft - walk;
+});
