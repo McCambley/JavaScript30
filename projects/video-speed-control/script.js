88cb46ed9c4dfb4acd750e8d6f9c00f68096b6a8
McCambley <jakemccamb16@gmail.com>
2021-09-22 22:21:23 -0400

    Dev: complete custom slider

--- projects/video-speed-control/script.js
+++ projects/video-speed-control/script.js
@@ -1,1 +1,22 @@
-console.info("Hello, World!");
+console.info('Hello, World!');
+
+const speed = document.querySelector('.content__slider');
+const bar = document.querySelector('.content__slider-bar');
+const video = document.querySelector('.content__video');
+
+speed.addEventListener('mousemove', function (e) {
+  // const bodyRect = document.body.getBoundingClientRect();
+  // const speedRect = this.getBoundingClientRect();
+  // const speedOffset = speedRect.top - bodyRect.top;
+  const top = Math.round(this.getBoundingClientRect().top);
+  let y = e.clientY - top;
+  const percent = Math.round((Math.abs(y - this.offsetHeight) / this.offsetHeight) * 100);
+  const min = 0.25;
+  const max = 3.5;
+  const height = percent + '%';
+  const playbackRate = (percent / 100) * (max - min) + min;
+  bar.textContent = playbackRate.toFixed(1);
+  video.playbackRate = playbackRate;
+  bar.style.height = height;
+  bar.style.minHeight = '15%';
+});
