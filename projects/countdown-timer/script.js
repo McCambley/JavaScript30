ad8da44d26441f55499924195aa73848f0b4bc46
McCambley <jakemccamb16@gmail.com>
2021-09-24 23:49:00 -0400

    Dev: complete timer

--- projects/countdown-timer/script.js
+++ projects/countdown-timer/script.js
@@ -1,1 +1,50 @@
-console.info("Hello, World!");
+let countdown;
+const timerDisplay = document.querySelector('.content__countdown');
+const endTime = document.querySelector('.content__termination');
+const buttons = document.querySelectorAll('.content__button');
+
+function timer(seconds) {
+  clearInterval(countdown);
+  const now = Date.now();
+  const then = now + seconds * 1000;
+  displayTimeLeft(seconds);
+  displayEndTime(then);
+
+  countdown = setInterval(() => {
+    const secondsLeft = Math.round((then - Date.now()) / 1000);
+    if (secondsLeft < 0) {
+      clearInterval(countdown);
+    }
+    displayTimeLeft(secondsLeft);
+  }, 1000);
+}
+
+function displayTimeLeft(seconds) {
+  const minutes = Math.floor(seconds / 60);
+  let overflowSeconds = seconds % 60;
+  const adjustedSeconds = overflowSeconds < 10 ? `0${overflowSeconds}` : overflowSeconds;
+  const display = `${minutes}:${adjustedSeconds}`;
+  timerDisplay.textContent = display;
+  document.title = display;
+}
+
+function displayEndTime(timeStamp) {
+  const end = new Date(timeStamp);
+  const hour = end.getHours();
+  const minutes = end.getMinutes();
+  const adjustedHour = hour > 12 ? hour - 12 : hour;
+  const adjustedMinutes = minutes < 10 ? `0${minutes}` : minutes;
+  endTime.textContent = `Timer done at ${adjustedHour}:${adjustedMinutes}`;
+}
+
+buttons.forEach((button) => {
+  button.addEventListener('click', function () {
+    timer(this.dataset.time);
+  });
+});
+
+document.countdown.addEventListener('submit', function (evt) {
+  evt.preventDefault();
+  timer(this.minutes.value * 60);
+  this.reset();
+});
