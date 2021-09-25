afae29c3e38b0b7ce9e138a2e679f4f697f1e2a5
McCambley <jakemccamb16@gmail.com>
2021-09-25 00:38:56 -0400

    Dev: add timer ux

--- projects/countdown-timer/script.js
+++ projects/countdown-timer/script.js
@@ -1,10 +1,16 @@
 let countdown;
+const content = document.querySelector('.content');
 const timerDisplay = document.querySelector('.content__countdown');
+const options = document.querySelector('.content__options');
+const stop = document.querySelector('.content__reset');
 const endTime = document.querySelector('.content__termination');
 const buttons = document.querySelectorAll('.content__button');
+const resetButton = document.querySelector('.content__reset-button');
 
 function timer(seconds) {
   clearInterval(countdown);
+  stop.classList.remove('hide');
+  options.classList.add('hide');
   const now = Date.now();
   const then = now + seconds * 1000;
   displayTimeLeft(seconds);
@@ -12,10 +18,15 @@ function timer(seconds) {
 
   countdown = setInterval(() => {
     const secondsLeft = Math.round((then - Date.now()) / 1000);
+    displayTimeLeft(secondsLeft);
     if (secondsLeft < 0) {
       clearInterval(countdown);
+      content.classList.add('content_complete');
+      timerDisplay.style.marginBottom = '12px';
+      timerDisplay.textContent = '🎉';
+      endTime.textContent = 'Done!';
+      document.title = 'Done!';
     }
-    displayTimeLeft(secondsLeft);
   }, 1000);
 }
 
@@ -37,11 +48,22 @@ function displayEndTime(timeStamp) {
   endTime.textContent = `Timer done at ${adjustedHour}:${adjustedMinutes}`;
 }
 
+function resetTimer() {
+  clearInterval(countdown);
+  stop.classList.add('hide');
+  options.classList.remove('hide');
+  content.classList.remove('content_complete');
+  timerDisplay.textContent = '00:00';
+  endTime.textContent = 'Select time...';
+}
+
 buttons.forEach((button) => {
   button.addEventListener('click', function () {
     timer(this.dataset.time);
   });
 });
+
+resetButton.addEventListener('click', resetTimer);
 
 document.countdown.addEventListener('submit', function (evt) {
   evt.preventDefault();
