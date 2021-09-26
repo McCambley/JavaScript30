92222f912a03cc611df0eeca70462f0b852ad558
McCambley <jakemccamb16@gmail.com>
2021-09-25 22:28:21 -0400

    Dev: complete target game

--- projects/box-race/script.js
+++ projects/box-race/script.js
@@ -1,7 +1,18 @@
 const squares = document.querySelectorAll('.content__square');
+const title = document.querySelector('.title');
+const subtitle = document.querySelector('.subtitle');
 const score = document.querySelector('.subtitle__score');
+const easyButton = document.querySelector('.start__button_easy');
+const mediumButton = document.querySelector('.start__button_medium');
+const hardButton = document.querySelector('.start__button_hard');
 let scoreCount = 0;
+let minTime = 500;
+let maxTime = 3000;
 let lastSquare;
+let currentGame;
+let gameTimer;
+
+gameOver = false;
 
 function randomTime(min, max) {
   return Math.round(Math.random() * (max - min) + min);
@@ -19,46 +30,95 @@ function randomSquare(squares) {
 }
 
 function showTarget() {
-  const time = randomTime(500, 3000);
+  const time = randomTime(minTime, maxTime);
   const square = randomSquare(squares);
   square.classList.add('content__square_active');
-  setTimeout(() => {
+  currentGame = setTimeout(() => {
     square.classList.remove('content__square_active');
-    showTarget();
+    if (!gameOver) showTarget();
   }, time);
+}
+
+function recordSuccess(evt) {
+  if (this.classList.contains('content__square_active')) {
+    this.classList.remove('content__square_active');
+    this.classList.add('content__square_success');
+    setTimeout(() => {
+      this.classList.remove('content__square_success');
+      // scoreCount++;
+      updateScore(evt);
+      score.textContent = scoreCount;
+    }, 500);
+  }
+}
+
+function startGame(min, max) {
+  hideTitle();
+  resetBoard();
+  minTime = min;
+  maxTime = max;
+  scoreCount = 0;
+  score.textContent = scoreCount;
+  gameOver = false;
+  showTarget();
+  gameTimer = setTimeout(() => {
+    gameOver = true;
+    subtitle.classList.add('grow');
+    subtitle.classList.add('grow');
+  }, 10000);
+}
+
+function resetBoard() {
+  squares.forEach((square) => {
+    square.classList.remove('content__square_active');
+    square.classList.remove('content__square_success');
+  });
+  clearTimeout(currentGame);
+  clearTimeout(gameTimer);
+}
+
+function updateScore(event) {
+  if (!event.isTrusted) return;
+  scoreCount++;
+}
+
+function hideTitle() {
+  title.classList.add('hide');
+  subtitle.classList.remove('hide');
+  subtitle.classList.remove('grow');
 }
 
 squares.forEach((square) => {
   square.addEventListener('click', recordSuccess);
 });
 
-function recordSuccess() {
-  if (this.classList.contains('content__square_active')) {
-    this.classList.remove('content__square_active');
-    this.classList.add('content__square_success');
-    setTimeout(() => {
-      this.classList.remove('content__square_success');
-      scoreCount++;
-      score.textContent = scoreCount;
-    }, 500);
-  }
-}
+easyButton.addEventListener('click', () => {
+  startGame(500, 2000);
+});
 
-function toggleSuccess() {
-  if (!this.classList.contains('content__square_active') && !this.classList.contains('content__square_success')) {
-    this.classList.add('content__square_active');
-    return;
-  } else if (!this.classList.contains('content__square_success')) {
-    this.classList.remove('content__square_active');
-    this.classList.add('content__square_success');
-    setTimeout(() => {
-      this.classList.remove('content__square_success');
-      scoreCount++;
-      score.textContent = scoreCount;
-    }, 1000);
-    return;
-  } else {
-    this.classList.remove('content__square_success');
-    return;
-  }
-}
+mediumButton.addEventListener('click', () => {
+  startGame(300, 1500);
+});
+
+hardButton.addEventListener('click', () => {
+  startGame(100, 1000);
+});
+
+// function toggleSuccess() {
+//   if (!this.classList.contains('content__square_active') && !this.classList.contains('content__square_success')) {
+//     this.classList.add('content__square_active');
+//     return;
+//   } else if (!this.classList.contains('content__square_success')) {
+//     this.classList.remove('content__square_active');
+//     this.classList.add('content__square_success');
+//     setTimeout(() => {
+//       this.classList.remove('content__square_success');
+//       scoreCount++;
+//       score.textContent = scoreCount;
+//     }, 1000);
+//     return;
+//   } else {
+//     this.classList.remove('content__square_success');
+//     return;
+//   }
+// }
